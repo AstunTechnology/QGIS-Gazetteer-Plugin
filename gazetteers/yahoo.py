c3b720196c3e8758032bd8cafccd20e7ed4c5622
Matt Walker <walkermatt@longwayaround.org.uk>
2013-10-28 09:09:33 +0000

    Use unused centroid element to get long/lat in yahoo

--- gazetteers/yahoo.py
+++ gazetteers/yahoo.py
@@ -13,11 +13,11 @@ def parseRequestResults(data):
 
     tree = ElementTree.fromstring(data)
     for item in tree.findall(schema('place')):
-        centroid = item.find(schema('centroid'))
         result = namedtuple('Result',['description','x','y','zoom', 'epsg'])
         result.description = "%s, %s" % (item.find(schema('name')).text, item.find(schema('country')).text)
-        result.x = float(item.find(schema('centroid')).find(schema('longitude')).text)
-        result.y = float(item.find(schema('centroid')).find(schema('latitude')).text)
+        centroid = item.find(schema('centroid'))
+        result.x = float(centroid.find(schema('longitude')).text)
+        result.y = float(centroid.find(schema('latitude')).text)
         result.zoom = 50000
         result.epsg = 4326 
         yield result
