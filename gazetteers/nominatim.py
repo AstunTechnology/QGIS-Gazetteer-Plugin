7624f9d3eaafe2b45be15ae1253da1d8acfb111f
Matt Walker <walkermatt@longwayaround.org.uk>
2013-10-28 19:48:18 +0000

    Minor PEP8 fixes

--- gazetteers/nominatim.py
+++ gazetteers/nominatim.py
@@ -3,21 +3,21 @@ from xml.etree import ElementTree
 
 url = "http://nominatim.openstreetmap.org/search"
 params = {
-            'q': '##searchstring##',
-            'limit': 50,
-            'format':'xml',
-            'addressdetails': 1,
-            'countrycodes':'gb'
-          }
-    
+    'q': '##searchstring##',
+    'limit': 50,
+    'format': 'xml',
+    'addressdetails': 1,
+    'countrycodes': 'gb'
+}
+
+
 def parseRequestResults(data):
     tree = ElementTree.fromstring(data)
     for place in tree.iter('place'):
-        result = namedtuple('Result',['description','x','y','zoom', 'epsg'])
+        result = namedtuple('Result', ['description', 'x', 'y', 'zoom', 'epsg'])
         result.description = place.attrib['display_name']
         result.x = float(place.attrib['lon'])
         result.y = float(place.attrib['lat'])
         result.zoom = 50000
-        result.epsg = 4326 
+        result.epsg = 4326
         yield result
-         
